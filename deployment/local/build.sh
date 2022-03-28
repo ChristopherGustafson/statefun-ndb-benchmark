7113af9656e9a38fad0769229686f09bb25522af
ChristopherGustafson <christopher.gustafson@outlook.com>
2022-03-28 16:16:37 +0200

    Fixed incorrect commands and log bugs

--- deployment/local/build.sh
+++ deployment/local/build.sh
@@ -2,11 +2,11 @@
 
 # Build output consumer
 echo "Building data stream generator..."
-(cd data-stream-generator/;sbt build)
+(cd data-stream-generator/;sbt compile)
 
 # Build embedded StateFun job
 echo "Building embedded StateFun job..."
 (cd shoppingcart-embedded/;mvn clean package)
 
 echo "Building output-consumer..."
-(cd output-consumer/;sbt build)
+(cd output-consumer/;sbt compile)
--- deployment/local/run.sh
+++ deployment/local/run.sh
@@ -8,25 +8,25 @@ docker rm kafka
 docker stop zookeeper
 docker rm zookeeper
 
-export BenchmarkJobName = "BenchmarkJob: "
+export BenchmarkJobName="BenchmarkJob: "
 
-echo "${BenchmarkJobName}Starting Kafka...\n"
+echo "${BenchmarkJobName}Starting Kafka..."
 ./kafka-cluster-tools/setup-kafka.sh
 
 # Start Flink Cluster, expects Flink build to available at /deployment/flink/build
-echo "${BenchmarkJobName}Starting Flink Cluster...\n"
+echo "${BenchmarkJobName}Starting Flink Cluster..."
 ./deployment/flink/build/bin/start-cluster.sh
 # Wait for startup
 sleep 10
 
 # Start StateFun-runtime
-echo "${BenchmarkJobName}Starting StateFun runtime...\n"
+echo "${BenchmarkJobName}Starting StateFun runtime..."
 ./deployment/flink/build/bin/flink run -c org.apache.flink.statefun.flink.core.StatefulFunctionsJob shoppingcart-embedded/target/shoppingcart-embedded-1.0-SNAPSHOT-jar-with-dependencies.jar &
 # Wait for startup
 sleep 10
 
 # Start data-stream-generator
-echo "${BenchmarkJobName}Starting data-stream-generator...\n"
+echo "${BenchmarkJobName}Starting data-stream-generator..."
 cd data-stream-generator
 source ./setEnvVariables.sh
 sbt run
@@ -36,7 +36,7 @@ cd ..
 ./deployment/flink/build/bin/stop-cluster.sh
 
 # Run output consumer
-echo "${BenchmarkJobName}Starting output consumer...\n"
+echo "${BenchmarkJobName}Starting output consumer..."
 cd output-consumer
 source ./setEnvVariables.sh
 sbt run
