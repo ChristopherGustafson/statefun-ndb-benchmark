bc3e58d08d28fb4c4e0deaff9ea9973b6a641f6e
ChristopherGustafson <christopher.gustafson@outlook.com>
2022-04-01 16:06:24 +0200

    Added sql scripts for initializing and clearing rondb

---
+++ deployment/ndb-utils/clear_db.sql
@@ -0,0 +1,11 @@
+DELETE FROM key_value_state;
+DELETE FROM key_value_state_committed;
+DELETE FROM key_value_state_snapshot;
+DELETE FROM list_key_value_state_snapshot;
+DELETE FROM list_state_attrib;
+DELETE FROM list_state_attrib_committed;
+DELETE FROM list_value_state;
+DELETE FROM list_value_state_committed;
+DELETE FROM map_key_value_state;
+DELETE FROM map_key_value_state_committed;
+DELETE FROM map_key_value_state_snapshot;
