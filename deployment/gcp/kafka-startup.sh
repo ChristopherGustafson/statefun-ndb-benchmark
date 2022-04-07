77808c43cff6f1ca07abc0fe55b708fbf6932827
ChristopherGustafson <christopher.gustafson@outlook.com>
2022-04-07 14:15:17 +0200

    Added Kafka setup to gcp script

---
+++ deployment/gcp/kafka-startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup java
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jre
+sudo apt-get clean
+
+# Download kafka
+wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
+tar -xzf kafka_2.13-3.1.0.tgz
+cd kafka_2.13-3.1.0
+
+# Start ZK and Kafka
+export KAFKA_ADDRESS=$(hostname -I | head -n1 | awk '{print $1;}'):9092
+
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/kafka-server-start.sh config/server.properties --override advertised.listeners=PLAINTEXT://$KAFKA_ADDRESS --override listeners=PLAINTEXT://$KAFKA_ADDRESS &
+
+bin/kafka-topics.sh --create --topic "add-to-cart" --bootstrap-server $KAFKA_ADDRESS
+bin/kafka-topics.sh --create --topic "checkout" --bootstrap-server $KAFKA_ADDRESS
+bin/kafka-topics.sh --create --topic "restock" --bootstrap-server $KAFKA_ADDRESS
+bin/kafka-topics.sh --create --topic "add-confirm" --bootstrap-server $KAFKA_ADDRESS
+bin/kafka-topics.sh --create --topic "receipts" --bootstrap-server $KAFKA_ADDRESS
+exit 0
+
+
--- deployment/gcp/run.sh
+++ deployment/gcp/run.sh
@@ -3,11 +3,16 @@
 # Config variables
 export JOBMANAGER_NAME=statefun-benchmark-jobmanager
 export TASKMANAGER_NAME=statefun-benchmark-taskmanager
+export KAFKA_NAME=statefun-benchmark-kafka
+export KAFKA_DOCKER_IMAGE=wurstmeister/kafka:2.12-2.1.1
+export ZK_DOCKER_IMAGE=jplock/zookeeper
 export N_WORKERS="${1:-1}"
-export GCP_IMAGE=ubuntu-minimal-2004-focal-v20220325
+export GCP_IMAGE=ubuntu-minimal-2004-focal-v20220406
 export GCP_IMAGE_PROJECT=ubuntu-os-cloud
 export GCP_MACHINE_TYPE=e2-standard-2
 
+export COS_IMAGE=cos-stable-97-16919-29-5
+export COS_IMAGE_PROJECT=cos-project
 
 echo "Running StateFun Rondb benchmark with $N_WORKERS TaskManagers"
 
@@ -16,15 +21,20 @@ echo "Running StateFun Rondb benchmark with $N_WORKERS TaskManagers"
 # RONDB_ADDRESS=`gcloud compute instances describe statefun-benchmark-rondbhead --format='get(networkInterfaces[0].networkIP)'`
 
 # ************** KAFKA SETUP **************
+echo "Creating VM for Kafka"
 
-#KAFKA_ADDRESS="localhost:9092"
-#
-## Set kafka address in StateFun Job
-#echo "bootstrap.servers=$KAFKA_ADDRESS" > shoppingcart-embedded/src/main/resources/config.properties
+gcloud compute instances create $KAFKA_NAME --image=$GCP_IMAGE --image-project=$GCP_IMAGE_PROJECT --machine-type=$GCP_MACHINE_TYPE --tags http-server,https-server
+echo "Waiting for Kafka VM startup"
+sleep 40
 
+gcloud compute ssh $KAFKA_NAME -- bash -s < deployment/gcp/kafka-startup.sh &
+
+# ************** FLINK SETUP **************
 echo "Creating VM for JobManager"
 gcloud compute instances create $JOBMANAGER_NAME --image=$GCP_IMAGE --image-project=$GCP_IMAGE_PROJECT --machine-type=$GCP_MACHINE_TYPE --tags http-server,https-server
 JOBMANAGER_ADDRESS=`gcloud compute instances describe $JOBMANAGER_NAME --format='get(networkInterfaces[0].networkIP)'`
+echo "Waiting for JobManager VM startup..."
+sleep 40
 
 echo "Packaging Flink Build"
 cp deployment/gcp/flink-utils/rocksdb_flink-conf.yaml.tmpl deployment/flink/build/conf/flink-conf.yaml
@@ -37,26 +47,40 @@ state.checkpoints.dir: file:///tmp/flinkcheckpoints
 tar -C deployment/flink -czvf flink.tar.gz build
 
 echo "Setting up Flink JobManager"
-sleep 10
 gcloud compute scp flink.tar.gz $JOBMANAGER_NAME:~
 gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink-utils/jobmanager_setup.sh
 
 echo "Creating VMs for TaskManagers"
-for i in $(seq 1 $N_WORKERS);
+for i in $(seq 1 $N_WORKERS)
 do
   WORKER_NAME="$TASKMANAGER_NAME-$i"
   echo "Setting up $WORKER_NAME"
   gcloud compute instances create $WORKER_NAME --image=$GCP_IMAGE --image-project=$GCP_IMAGE_PROJECT --machine-type=$GCP_MACHINE_TYPE --tags http-server,https-server
-  # Sleep to let vm start properly
-  sleep 10
+done
+
+# Sleep to let vm start properly
+sleep 40
+echo "Initializing TaskManagers"
+for i in $(seq 1 $N_WORKERS)
+do
   gcloud compute scp flink.tar.gz $WORKER_NAME:~
   gcloud compute ssh $WORKER_NAME -- bash -s < deployment/gcp/flink-utils/taskmanager_setup.sh
 done
+
+export KAFKA_ADDRESS=`gcloud compute instances describe $KAFKA_NAME --format='get(networkInterfaces[0].networkIP)'`
+echo "Building StateFun job"
+echo "bootstrap.servers=$KAFKA_ADDRESS:9092" > shoppingcart-embedded/src/main/resources/config.properties
+(cd shoppingcart-embedded/;mvn clean package)
 
 echo "Running StateFun runtime"
 gcloud compute scp shoppingcart-embedded/target/shoppingcart-embedded-1.0-SNAPSHOT-jar-with-dependencies.jar $JOBMANAGER_NAME:~
 gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink-utils/run_statefun.sh
 
-
-
-
+# Clean up
+#echo "Deleting VM instances"
+#gcloud compute instances delete $JOBMANAGER_NAME
+#for i in $(seq 1 $N_WORKERS);
+#do
+#  WORKER_NAME="$TASKMANAGER_NAME-$i"
+#  gcloud compute instances delete $WORKER_NAME
+#end
