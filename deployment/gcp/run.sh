1ae5cc9cee90698946d7e31b8d0a3c3ad7f40ffa
ChristopherGustafson <christopher.gustafson@outlook.com>
2022-04-08 15:05:19 +0200

    Fixed new file paths and instance name prefix

--- deployment/gcp/run.sh
+++ deployment/gcp/run.sh
@@ -2,25 +2,27 @@
 
 # Config variables
 
+# General Config
+export NAME_PREFIX=statefun-benchmark-
+
 # GCP config
 export GCP_IMAGE=ubuntu-minimal-2004-focal-v20220406
 export GCP_IMAGE_PROJECT=ubuntu-os-cloud
 export GCP_MACHINE_TYPE=e2-standard-2
 
 # Kafka config
-export KAFKA_NAME=kafka
+export KAFKA_NAME=${NAME_PREFIX}kafka
 export KAFKA_DOCKER_IMAGE=wurstmeister/kafka:2.12-2.1.1
 
 # Flink config
-export JOBMANAGER_NAME=jobmanager
-export TASKMANAGER_NAME=taskmanager
+export JOBMANAGER_NAME=${NAME_PREFIX}jobmanager
+export TASKMANAGER_NAME=${NAME_PREFIX}taskmanager
 # Set state backend, should be either 'ndb' or 'rocksdb'
 export N_WORKERS="${1:-1}"
 export STATE_BACKEND="${2:-"rocksdb"}"
 
-
-# Other config
-export DATA_UTILS_NAME=data-utils
+# Data utils config
+export DATA_UTILS_NAME=${NAME_PREFIX}data-utils
 
 echo "Running StateFun Rondb benchmark with $N_WORKERS TaskManagers"
 
@@ -39,7 +41,7 @@ gcloud compute instances create $KAFKA_NAME --image=$GCP_IMAGE --image-project=$
 echo "Waiting for Kafka VM startup"
 sleep 40
 
-gcloud compute ssh $KAFKA_NAME -- bash -s < deployment/gcp/kafka-startup.sh &
+gcloud compute ssh $KAFKA_NAME -- bash -s < deployment/gcp/kafka/kafka-startup.sh &
 export KAFKA_ADDRESS=`gcloud compute instances describe $KAFKA_NAME --format='get(networkInterfaces[0].networkIP)'`
 
 # *
@@ -56,7 +58,7 @@ bootstrap.servers = $KAFKA_ADDRESS:9092
 " > data-utils/config.properties
 tar -czvf data-utils.tar.gz data-utils
 gcloud compute scp data-utils.tar.gz $DATA_UTILS_NAME:~
-gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/data-utils-startup.sh
+gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/data-utils/data-utils-startup.sh
 
 # *
 # ************** FLINK SETUP **************
@@ -73,18 +75,17 @@ echo "Packaging Flink Build"
 if [ "$STATE_BACKEND" = "rocksdb" ];
 then
   echo "Setting state backend to RocksDB"
-  ./deployment/local/set-rocksdb-backend.sh
+  cp deployment/gcp/flink/rocksdb_flink-conf.yaml.tmpl deployment/flink/build/conf/flink-conf.yaml
 fi
 if [ "$STATE_BACKEND" = "ndb" ];
 then
   echo "Setting state backend to NDB"
-  ./deployment/local/set-rocksdb-backend.sh
+  cp deployment/gcp/flink/ndb_flink-conf.yaml.tmpl deployment/flink/build/conf/flink-conf.yaml
   echo "
   state.backend.ndb.connectionstring: $RONDB_ADDRESS
   " >> deployment/flink/build/conf/flink-conf.yaml
 fi
 
-cp deployment/gcp/flink-utils/rocksdb_flink-conf.yaml.tmpl deployment/flink/build/conf/flink-conf.yaml
 echo "
 jobmanager.rpc.address: $JOBMANAGER_ADDRESS
 parallelism.default: $N_WORKERS
@@ -95,7 +96,7 @@ tar -C deployment/flink -czvf flink.tar.gz build
 
 echo "Setting up Flink JobManager"
 gcloud compute scp flink.tar.gz $JOBMANAGER_NAME:~
-gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink-utils/jobmanager_setup.sh
+gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink/jobmanager_setup.sh
 
 echo "Creating VMs for TaskManagers"
 for i in $(seq 1 $N_WORKERS)
@@ -112,7 +113,7 @@ for i in $(seq 1 $N_WORKERS)
 do
   WORKER_NAME="$TASKMANAGER_NAME-$i"
   gcloud compute scp flink.tar.gz $WORKER_NAME:~
-  gcloud compute ssh $WORKER_NAME -- bash -s < deployment/gcp/flink-utils/taskmanager_setup.sh
+  gcloud compute ssh $WORKER_NAME -- bash -s < deployment/gcp/flink/taskmanager_setup.sh
 done
 
 echo "Building StateFun job"
@@ -121,7 +122,7 @@ echo "bootstrap.servers=$KAFKA_ADDRESS:9092" > shoppingcart-embedded/src/main/re
 
 echo "Running StateFun runtime"
 gcloud compute scp shoppingcart-embedded/target/shoppingcart-embedded-1.0-SNAPSHOT-jar-with-dependencies.jar $JOBMANAGER_NAME:~
-gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink-utils/run_statefun.sh &
+gcloud compute ssh $JOBMANAGER_NAME -- bash -s < deployment/gcp/flink/run_statefun.sh &
 echo "Waiting for StateFun runtime startup"
 sleep 30
 
@@ -129,23 +130,12 @@ sleep 30
 # ************** BENCHMARK RUN **************
 # *
 echo "Starting Data Generator"
-gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/run-data-generator.sh
+gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/data-utils/run-data-generator.sh
 echo "Data Generator Finished"
-#gcloud compute ssh $DATA_UTILS_NAME --command 'cd data-utils && nohup python3 produce_events.py &'
-## Let it run for 60 seconds
-#sleep 60
-#echo "Stopping Data Generator"
-#gcloud compute ssh $DATA_UTILS_NAME --command 'pkill -f produce_events.py'
 
 echo "Starting Output Consumer"
-gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/run-output-consumer.sh
+gcloud compute ssh $DATA_UTILS_NAME -- bash -s < deployment/gcp/data-utils/run-output-consumer.sh
 echo "Output Consumer Finished"
-
-#gcloud compute ssh $DATA_UTILS_NAME --command 'cd data-utils && python3 output_consumer.py' &
-## Let it run for 60 seconds
-#sleep 60
-#echo "Stopping Output Consumer"
-#gcloud compute ssh $DATA_UTILS_NAME --command 'pkill -f output_consumer.py'
 
 # Copy data file to local
 NOW="$(date +'%d-%m-%Y_%H_%M')"
