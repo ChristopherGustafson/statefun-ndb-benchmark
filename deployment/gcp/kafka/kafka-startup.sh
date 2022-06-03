fc1ea9c526794c704685381c0054ad9be4cb921d
Christopher Gustafson <christopher.gustafson@outlook.com>
2022-06-03 09:23:59 +0200

    Changed kafka version for compatability and changed to 8 topic partitions

--- deployment/gcp/kafka/kafka-startup.sh
+++ deployment/gcp/kafka/kafka-startup.sh
@@ -6,9 +6,12 @@ sudo apt-get -y install openjdk-8-jre
 sudo apt-get clean
 
 # Download kafka
-wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
-tar -xzf kafka_2.13-3.1.0.tgz
-cd kafka_2.13-3.1.0
+#wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
+#tar -xzf kafka_2.13-3.1.0.tgz
+#cd kafka_2.13-3.1.0
+wget https://archive.apache.org/dist/kafka/2.4.1/kafka_2.12-2.4.1.tgz
+tar -xzf kafka_2.12-2.4.1.tgz
+cd kafka_2.12-2.4.1
 
 # Start ZK and Kafka
 export KAFKA_ADDRESS=$(hostname -I | head -n1 | awk '{print $1;}'):9092
@@ -16,11 +19,12 @@ export KAFKA_ADDRESS=$(hostname -I | head -n1 | awk '{print $1;}'):9092
 bin/zookeeper-server-start.sh config/zookeeper.properties &
 bin/kafka-server-start.sh config/server.properties --override advertised.listeners=PLAINTEXT://$KAFKA_ADDRESS --override listeners=PLAINTEXT://$KAFKA_ADDRESS &
 
-bin/kafka-topics.sh --create --topic "add-to-cart" --bootstrap-server $KAFKA_ADDRESS --partitions 32
-bin/kafka-topics.sh --create --topic "checkout" --bootstrap-server $KAFKA_ADDRESS --partitions 32
-bin/kafka-topics.sh --create --topic "restock" --bootstrap-server $KAFKA_ADDRESS --partitions 32
-bin/kafka-topics.sh --create --topic "add-confirm" --bootstrap-server $KAFKA_ADDRESS --partitions 32
-bin/kafka-topics.sh --create --topic "receipts" --bootstrap-server $KAFKA_ADDRESS --partitions 32
-exit 0
+bin/kafka-topics.sh --create --topic "add-to-cart" --bootstrap-server $KAFKA_ADDRESS --partitions 8
+bin/kafka-topics.sh --create --topic "checkout" --bootstrap-server $KAFKA_ADDRESS --partitions 8
+bin/kafka-topics.sh --create --topic "restock" --bootstrap-server $KAFKA_ADDRESS --partitions 8
+bin/kafka-topics.sh --create --topic "add-confirm" --bootstrap-server $KAFKA_ADDRESS --partitions 8
+bin/kafka-topics.sh --create --topic "receipts" --bootstrap-server $KAFKA_ADDRESS --partitions 8
+
+
 
 
